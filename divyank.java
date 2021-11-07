d0857509675ed9f118d87b338dc799166a58824c
“divyankrai” <divyankrai@gmail.com>
2021-11-07 07:37:07 +0000

    New code added by Divyank

--- divyank.java
+++ divyank.java
@@ -1,2 +1,12 @@
-Hello world
-this is just test system.
+public class MyFirstCode {
+
+	public static void main(String[] args) {
+		int a = 20, b = 200, c;
+		c = a + b;
+		System.out.println("Hello I am back with result");
+        System.out.println("Result:" +c);
+        
+	}
+
+}
+
