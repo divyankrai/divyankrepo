0595463cce91ede7ea130c133320ce732ffd1291
“divyankrai” <divyankrai@gmail.com>
2021-11-07 07:40:26 +0000

    New files added by divyank

---
+++ divyank2.java
@@ -0,0 +1,12 @@
+public class MyFirstCode {
+
+	public static void main(String[] args) {
+		int a = 20, b = 200, c;
+		c = a + b;
+		System.out.println("Hello I am back with result");
+        System.out.println("Result:" +c);
+        
+	}
+
+}
+
