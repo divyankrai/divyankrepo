23a1071fe5ace295eef5ebd1ab44d1e41b293e9d
Divyank Rai <divyankrai@gmail.com>
2021-11-08 03:19:06 +0000

    Changes made by Dev2

--- divyank.java
+++ divyank.java
@@ -5,7 +5,7 @@ public class MyFirstCode {
 		c = a + b;
 		System.out.println("Hello I am back with result");
         System.out.println("Result:" +c);
-        
+       		System.out.println("I am dev2 and I am batman"); //Changes made by Dev2
 	}
 
 }
